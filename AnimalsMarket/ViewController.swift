511a8eca27e6883296031dc9ed539183265ff0c0
何　暁川 <>
2020-04-27 23:40:36 +0900

    init

---
+++ AnimalsMarket/ViewController.swift
@@ -0,0 +1,21 @@
+////  ViewController.swift
+//  AnimalsMarket
+//
+//  Created by hexch on 2020/04/27.
+//  github:https://github.com/hexch
+//
+//  Copyright © 2020 hexch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
